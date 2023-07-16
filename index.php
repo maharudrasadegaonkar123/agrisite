5fcacc92738b4dd5f012668ae6477455500e13c0
maharudrasadegaonkar123 <139707020+maharudrasadegaonkar123@users.noreply.github.com>
2023-07-16 23:06:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php include("header.php"); ?>
+<div id="wowslider-container1">
+  <div class="ws_images">
+    <ul>
+      <li><img src="data1/images/slider1.jpg"  alt="" title=""/></li>
+      <li><img src="data1/images/slider2.jpg" alt="" title="" /></li>
+      <li><img src="data1/images/slider3.jpg" alt="" title="" /></li>
+      <li><img src="data1/images/slider4.jpg" alt="" title="" /></li>
+    </ul>
+  </div>
+</div>
+    <div id="about" class="section wb">
+        <div class="container">
+            <div class="row">
+			  <h2 class="maintitle">Welcome To Rudra Creation</h2>
+                <div class="col-md-7">
+                    <div class="message-box">
+                      
+						 <p>Dear Distributors,</p>
+                        <p>Welcome to Rudra Creation<br><br>
+						 <br><br>
+						
+			         </p>
+                    </div><!-- end messagebox -->
+                </div><!-- end col -->
+
+                <div class="col-md-5">
+                    <div class="post-media wow fadeIn"><br>
+                        <img src="uploads/about_01.jpg" alt="" class="img-responsive img-rounded">
+                    </div><!-- end media -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+			
+			 <div class="row">
+                <div class="col-md-12">
+                    <div class="message-box">
+					
+					  <span class="lead">Best of Luck!</span></p>
+					
+					
+                    </div><!-- end messagebox -->
+                </div><!-- end col -->
+            </div>
+
+       
+        </div><!-- end container -->
+    </div><!-- end section -->
+	
+	<div class="parallax section noover" data-stellar-background-ratio="0.7" style="background-image:url('uploads/parallax_05.png');">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-6">
+                    <div class="customwidget text-left">
+                        <h2><span style="color:#ffffff; font-size:35px;font-weight: 600;">Why Choose Us ?<span></h2>
+                        <p></p>
+                        <ul class="list-inline">
+						    <li><i class="fa fa-check"></i> Our Profile</li>
+                            <li><i class="fa fa-check"></i> Development</li>
+                            <li><i class="fa fa-check"></i> Solutions</li>
+                            <li><i class="fa fa-check"></i> Quality Products</li>
+                        </ul><!-- end list -->
+                        <a href="#." data-scroll class="btn btn-light btn-radius btn-brd">View More</a>
+                    </div>
+                </div><!-- end col -->
+				<div class="col-md-6">
+                    <div class="text-center image-center hidden-sm hidden-xs">
+                        <img src="uploads/device_03.png" alt="" class="img-responsive wow fadeInUp">
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section --><br><br>
+	
+	<div class="parallax section parallax-off" data-stellar-background-ratio="0.9" style="background-image:url('uploads/parallax_04.jpg');">
+        <div class="container"><br><br>
+            <div class="row text-center stat-wrap">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                   <h2 style="color:#ffffff;font-size:36px;"><strong>We are the innovators in agricultural <span style="font-size:48px;color:#00FF00;">business</span></strong></h2>
+
+                </div><!-- end col -->
+
+                <div class="col-md-2 col-sm-6 col-xs-12">
+                   <span data-scroll class="global-radius icon_wrap effect-1"><img src="images/1s.png" alt="" class="img-responsive"></span>
+                </div><!-- end col -->
+
+                <div class="col-md-2 col-sm-6 col-xs-12">
+                    <span data-scroll class="global-radius icon_wrap effect-1"><img src="images/2s.png" alt="" class="img-responsive"></span>
+                </div><!-- end col -->
+
+                <div class="col-md-2 col-sm-6 col-xs-12">
+                   <span data-scroll class="global-radius icon_wrap effect-1"><img src="images/3s.png" alt="" class="img-responsive"></span>
+                </div><!-- end col -->
+            </div><!-- end row --><br><br><br><br>
+        </div><!-- end container -->
+    </div><!-- end section -->
+
+    
+	
+   <?php include("footer.php"); ?>
